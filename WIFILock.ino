8fec9d3e8958f6569e0fdad65b4da15c773c3ca1
Pondi1015 <pondi20902@gmail.com>
2021-01-08 20:59:24 +0800

    WIFI與伺服馬達試用

---
+++ WIFILock.ino
@@ -0,0 +1,135 @@
+#include <WiFiEsp.h>
+#include <Servo.h>
+
+#define LAN_SSID "FredMyisYEEFAN"
+#define LAN_PASS "YEE810643"
+
+WiFiEspServer server(80);
+int status = WL_IDLE_STATUS;     // the Wifi radio's status
+RingBuffer buf(32);
+char cmd[32];
+Servo myServo;
+
+int ledPin = 12;
+int servoPin = 8;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  myServo.attach(servoPin);
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  InitWiFi();
+}
+
+void loop() {
+  WiFiEspClient client = server.available();  // listen for incoming clients
+ 
+  if(client) {
+    Serial.println("New client");
+    buf.init();
+    int index = 0;
+    while(client.connected()) {
+      if(client.available()) {
+        char c = client.read();
+        buf.push(c);
+        cmd[index++] = c;
+        if (buf.endsWith(";")) {
+          cmd[index]=0;
+          execCmd(client);
+          index = 0;
+        }
+      }
+      delay(10);
+    }
+ 
+    // close the connection
+    client.stop();
+    Serial.println("Client disconnected");
+  }
+}
+
+void execCmd(WiFiEspClient client)
+{
+  Serial.println(cmd);
+  int index = 0;
+  while(cmd[index]!='@')
+    index++;
+  while(cmd[index]!=';')
+  {
+    if(cmd[index]=='A')
+    {
+      turnOnLed(13);
+      return;
+    }
+    else if(cmd[index]=='B')
+    {
+      turnOffLed(13);
+      return;
+    }
+    else if(cmd[index]=='C')//馬達的部分
+    {
+      int v = getInt(cmd, index+1);
+      Serial.println(v);
+      myServo.write(v%181);
+    }
+    index++;
+    delay(10);
+  }
+  Serial.println("Job done");
+}
+
+void turnOnLed(int ledpin)
+{
+  digitalWrite(ledpin, HIGH);  
+}
+
+void turnOffLed(int ledpin)
+{
+  digitalWrite(ledpin, LOW);  
+}
+
+int getInt(char *cmd, int index)
+{
+  int v = 0;
+  while(cmd[index]<'0'&&cmd[index]>'9')
+    index++;
+  while(cmd[index]>='0'&&cmd[index]<='9')
+  {
+    v = v*10+(cmd[index]-'0');
+    index++;
+  }
+  return v;
+}
+
+
+
+void InitWiFi()
+{
+  WiFi.init(&Serial1);
+
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    while (true); // don't continue
+  }
+
+  Serial.print("Attempting to start AP ");
+  Serial.println(LAN_SSID);
+
+  // start access point
+  status = WiFi.beginAP(LAN_SSID, 10, LAN_PASS, ENC_TYPE_WPA2_PSK);
+
+  Serial.println("Access point started");
+  printWifiStatus();
+
+  // start the web server on port 80
+  server.begin();
+  Serial.println("Server started");
+}
+
+void printWifiStatus()
+{
+  // print your WiFi shield's IP address
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address of This AP: ");
+  Serial.println(ip);
+ }
